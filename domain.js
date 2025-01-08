2099d4f16564a83f4a2402ceac37424f710ae381
ranzselebew <sbuat537@gmail.com>
2025-01-08 14:41:48 +0800

    pushwa

---
+++ domain.js
@@ -0,0 +1,5 @@
+module.exports = {
+    DOMAIN: "https://example.com", // Ganti dengan domain API Anda
+    API_KEY: "your_api_key_here",  // Ganti dengan API Key Anda
+    OWNER_ID: "123456789"          // Ganti dengan ID pemilik bot
+};
---
+++ undefined.js
@@ -0,0 +1,10 @@
+const global = {
+    superAdmin: [7943237510, 6644450387],
+    usernameOwner: "@cellasta",
+    botName: "𝙉𝙖𝙣𝙙𝙚𝙢𝙤ી",
+    botToken: "7787083055:AAGlvTJJYVFSngqJ5lEt_VwQYD_pTpzcRuA",
+    channelLink: "https://t.me/isengaja8",
+    preview: 'https://files.catbox.moe/z8wafh.mp4'
+}
+
+module.exports = global;
